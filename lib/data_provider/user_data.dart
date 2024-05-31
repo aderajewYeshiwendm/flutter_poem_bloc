d2466cd6e75930bcf57d9e299149aca2420df5f5
aderajewYeshiwendm <aderajewyeshiwendmg@gmail.com>
2024-05-31 22:37:30 +0300

    add user-data

---
+++ lib/data_provider/user_data.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/user.dart';
+
+class UserDataProvider {
+  final _baseUrl = 'http://localhost:3000/api/users/';
+  final http.Client httpClient;
+
+  UserDataProvider({required this.httpClient});
+
+  Future<List<User>> getUsers({required String token}) async {
+    final response =
+        await httpClient.get(Uri.parse(_baseUrl), headers: <String, String>{
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      final List<dynamic> userJson = jsonDecode(response.body);
+      return userJson.map((json) => User.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  Future<void> deleteUser(User user, {required String token}) async {
+    final http.Response response = await httpClient.delete(
+      Uri.parse('$_baseUrl${user.id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode != 204) {
+      throw Exception('Failed to delete user.');
+    }
+  }
+
+  Future<void> updateUser(User user, {required String token}) async {
+    final http.Response response = await httpClient.put(
+      Uri.parse('http://localhost:3000/api/allusers/${user.id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'password': user.password,
+        'role': user.role,
+      }),
+    );
+
+    if (response.statusCode != 204) {
+      throw Exception('Failed to update user.');
+    }
+  }
+}
