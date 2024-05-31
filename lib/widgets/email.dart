6192ddda726e5cb5962b896f8291697831c7b70b
aderajewYeshiwendm <aderajewyeshiwendmg@gmail.com>
2024-05-31 22:31:05 +0300

    add email widget

---
+++ lib/widgets/email.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/login_bloc/login_bloc.dart';
+import '../bloc/login_bloc/login_event.dart';
+import '../bloc/login_bloc/login_state.dart';
+
+class EmailField extends StatelessWidget {
+  const EmailField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: TextFormField(
+            initialValue: state.email,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+              hintText: 'your Email ...',
+              hintStyle: TextStyle(
+                color: Colors.grey.withOpacity(0.5),
+              ),
+              border: const OutlineInputBorder(),
+            ),
+            onChanged: (value) {
+              context.read<LoginBloc>().add(EmailChanged(value));
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your Email';
+              }
+              return null;
+            },
+          ),
+        );
+      },
+    );
+  }
+}
